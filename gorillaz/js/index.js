f9c3f4c12a98e229893555fae825688283f16e22
Zhenya <78028222+zhekkaq3@users.noreply.github.com>
2022-07-04 14:22:22 +0300

    Add files via upload
    added

---
+++ gorillaz/js/index.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger').addEventListener('click', function(){
+  document.querySelector('.burger span').classList.toggle('active');
+  document.querySelector('.menu').classList.toggle("animate");
+})
